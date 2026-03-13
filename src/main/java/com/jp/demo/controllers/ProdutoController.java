e1cbd88a9dab6aa742e8466af2afdbbf3f240f69
JP <joaopeu72@gmail.com>
2026-03-12 21:46:58 -0300

    controller completo

--- src/main/java/com/jp/demo/controllers/ProdutoController.java
+++ src/main/java/com/jp/demo/controllers/ProdutoController.java
@@ -1,10 +1,12 @@
 package com.jp.demo.controllers;
 
+import com.jp.demo.model.ProdutoModel;
 import com.jp.demo.repository.ProdutoRepository;
 import com.jp.demo.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/produto")
@@ -12,4 +14,24 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
+
+    @PostMapping
+    public ProdutoModel cadastrar(ProdutoModel produtoModel) {
+        return produtoService.cadastrar(produtoModel);
+    }
+
+    @GetMapping
+    public List<ProdutoModel> findAll() {
+        return produtoService.findAll();
+    }
+
+    @DeleteMapping
+    public void delete(@PathVariable Long id) {
+        produtoService.delete(id);
+    }
+
+    @PutMapping
+    public ProdutoModel atualizar(@PathVariable Long id, @RequestBody ProdutoModel produtoModel) {
+        return produtoService.atualizar(id, produtoModel);
+    }
 }
--- src/main/java/com/jp/demo/service/ProdutoService.java
+++ src/main/java/com/jp/demo/service/ProdutoService.java
@@ -14,22 +14,18 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    @PostMapping
     public ProdutoModel cadastrar(ProdutoModel produtoModel) {
         return produtoRepository.save(produtoModel);
     }
 
-    @GetMapping
     public List<ProdutoModel> findAll() {
         return produtoRepository.findAll();
     }
 
-    @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         produtoRepository.deleteById(id);
     }
 
-    @PutMapping("/{id}")
     public ProdutoModel atualizar(@PathVariable Long id, ProdutoModel produtoModel) {
         ProdutoModel produto = produtoRepository.findById(id).get();
 
@@ -39,4 +35,5 @@ public class ProdutoService {
 
         return produtoRepository.save(produto);
     }
+
 }
